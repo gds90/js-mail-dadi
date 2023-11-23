007e9ec124957ac2286d847a1b5756136decb4bd
gds90 <you@example.com>
2023-11-23 14:35:42 +0100

    Inserito esercizio Dadi in script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// MAIL
+
+// 1) creo la costante mail_list con 3 indirizzi email all'interno
+const mail_list = ['fabrizio@boolean.it', 'antonio@boolean.it', 'thomas@boolean.it']
+
+// 2) dichiaro una variabile e permetto all'utente di inserirne il valore tramite prompt;
+let mail = prompt('Inserisci la tua email:')
+
+// 3) tramite un ciclo For, confronto la mail inserita dall'utente con quelle presenti nell'array una alla volta;
+let flag = false;
+
+for (let i=0; i<mail_list.length; i++){
+
+    // 4) Se la mail inserita dall'utente è uguale ad una presente nell'arrey:
+    if (mail === mail_list[i]) {
+        flag = true;
+    }
+}
+
+if (flag == true){
+    // 4a) stampo in console un messaggio sull'esito positivo
+    console.log("La mail inserita è presente nella lista delle mail autorizzate, benvenuto!")
+} 
+else {
+    // 5) altrimenti stampo in console un messaggio sull'esito negativo
+    console.log('La mail inserita non è presente nella lista delle mail autorizzate, non puoi accedere.')
+}
