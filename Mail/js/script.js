0ba3bf9c186531abd863b408fb8a93f63ced21aa
gds90 <you@example.com>
2023-11-24 09:54:54 +0100

    Aggiunta di commenti in Mail script.js

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -4,9 +4,10 @@ const mail_list = ['fabrizio@boolean.it', 'antonio@boolean.it', 'thomas@boolean.
 // 2) dichiaro una variabile e permetto all'utente di inserirne il valore tramite prompt;
 let mail = prompt('Inserisci la tua email:')
 
-// 3) tramite un ciclo For, confronto la mail inserita dall'utente con quelle presenti nell'array una alla volta;
+// Dichiaro una variabile flag che mi permetterà di verificare se la mail è presente o meno nell'array
 let flag = false;
 
+// 3) tramite un ciclo For, confronto la mail inserita dall'utente con quelle presenti nell'array una alla volta;
 for (let i=0; i<mail_list.length; i++){
 
     // 4) Se la mail inserita dall'utente è uguale ad una presente nell'arrey:
@@ -15,11 +16,14 @@ for (let i=0; i<mail_list.length; i++){
     }
 }
 
+// Verifico il valore di flag per stampare il messaggio corretto
 if (flag == true){
+
     // 4a) stampo in console un messaggio sull'esito positivo
     console.log("La mail inserita è presente nella lista delle mail autorizzate, benvenuto!")
 } 
 else {
+
     // 5) altrimenti stampo in console un messaggio sull'esito negativo
     console.log('La mail inserita non è presente nella lista delle mail autorizzate, non puoi accedere.')
 }
